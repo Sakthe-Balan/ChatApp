7548d55902a0eb0ffd4dc8c424200bff145b79b9
Sakthe-Balan <sakthebalan2003@gmail.com>
2024-02-14 03:21:35 +0530

    made a chat interface with static messages

--- UI/wordlecup/src/Pages/Chat.tsx
+++ UI/wordlecup/src/Pages/Chat.tsx
@@ -1,14 +1,34 @@
 
 import React from 'react';
 import Sidebar from '../Components/Sidebar.tsx';
-
+import ChatMessage from '../Components/Chatmessage.tsx';
 
 const Chat: React.FC = () => {
   return (
     <div className="flex h-screen">
       <Sidebar />
 
-      
+      <div className="flex-grow p-4 overflow-y-auto bg-gray-100">
+        <div className="flex flex-col h-full border rounded-lg overflow-hidden bg-white">
+          <div className="flex-grow p-4 overflow-y-auto">
+            <ChatMessage user="User1" message="Hello!" />
+            <ChatMessage user="User2" message="Hi there!" />
+            <ChatMessage user="User1" message="How are you?" />
+            
+          </div>
+
+          <div className="p-4 border-t flex items-center">
+            <input
+              className="border rounded-md p-2 w-full focus:outline-none"
+              type="text"
+              placeholder="Type your message..."
+            />
+            <button className="bg-green-500 text-white py-2 px-4 rounded-md ml-2">
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
